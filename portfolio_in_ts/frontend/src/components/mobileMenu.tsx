e1bed7b1b7d991b72fd07be564633289d5e1cd97
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-07-14 18:51:28 +0545

    add some social media links and make responsive in mobile menu

--- portfolio_in_ts/frontend/src/components/mobileMenu.tsx
+++ portfolio_in_ts/frontend/src/components/mobileMenu.tsx
@@ -1,4 +1,4 @@
-import { AlignJustify } from "lucide-react"
+import { AlignJustify, Mail, MapPin } from "lucide-react";
 import {
   Sheet,
   SheetContent,
@@ -6,25 +6,127 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from "./ui/sheet"
+} from "./ui/sheet";
+import { Link } from "react-router";
+import { useState } from "react";
+import { Input } from "./ui/input";
+import { FaFacebook, FaGithub, FaInstagram } from "react-icons/fa";
 
 export default function MobileMenu() {
+  const [open, setOpen] = useState(false);
   return (
-    <Sheet>
-  <SheetTrigger><AlignJustify
-                className="flex md:hidden"
-                size={24}
-                strokeWidth={1.2}
-              /></SheetTrigger>
-  <SheetContent>
-    <SheetHeader>
-      <SheetTitle>Are you absolutely sure?</SheetTitle>
-      <SheetDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </SheetDescription>
-    </SheetHeader>
-  </SheetContent>
-</Sheet>
-  )
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger>
+        <AlignJustify className="flex md:hidden" size={24} strokeWidth={1.2} />
+      </SheetTrigger>
+      <SheetContent className="space-y-2">
+        <SheetHeader>
+          {/* menu title */}
+          <SheetTitle>Menu Bar</SheetTitle>
+
+         <div className="space-y-8">
+           {/* pages links */}
+          <SheetDescription className="flex flex-col gap-4 mt-8">
+            {menuItems.map((item, index) => (
+              <Link
+                to={item.url}
+                key={index}
+                onClick={() => setOpen(false)}
+                className="border-b text-center py-1 text-sm"
+              >
+                {item.text.toUpperCase()}
+              </Link>
+            ))}
+          </SheetDescription>
+
+           {/* Search section */}
+        <SheetDescription className="">
+          <Input
+            type="text"
+            className="border-0 border-b border-foreground focus-visible:ring-0 shadow-2xl  rounded-md text-sm"
+            placeholder="Search Here..."
+          />
+        </SheetDescription>
+        
+         </div>
+        </SheetHeader>
+
+       
+
+        {/* Link of the media  */}
+        <div className=" space-y-4 px-2">
+          {mediaItem.map((item, index) =>
+            item.icon ? (
+              <Link
+                to={item.url}
+                key={index}
+                className="flex  gap-2 items-center"
+              >
+                <item.icon size={24} className={`${item.iconColor}`} />
+                <SheetDescription className="font-semibold ">
+                  {item.itemName}{" "}
+                </SheetDescription>
+              </Link>
+            ) : null
+          )}
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 }
+
+const menuItems = [
+  {
+    url: "/",
+    text: "home",
+  },
+  {
+    url: "/project",
+    text: "project",
+  },
+  {
+    url: "/resume",
+    text: "resume",
+  },
+  {
+    url: "/contact",
+    text: "contact",
+  },
+  {
+    url: "/about",
+    text: "about me",
+  },
+];
+
+const mediaItem = [
+  {
+    url: "https://www.facebook.com/ritesh.lama.981107",
+    icon: FaFacebook,
+    iconColor: "text-blue-500",
+    itemName: "Facebook",
+  },
+  {
+    url: "https://www.instagram.com/riteshlama1027",
+    icon: FaInstagram,
+    iconColor: "text-red-500",
+    itemName: "Instagram",
+  },
+  {
+    url: "https://github.com/riteshtamang1027",
+    icon: FaGithub,
+    iconColor: "text-primary",
+    itemName: "GitHub",
+  },
+  {
+    url: "",
+    icon: Mail,
+    iconColor: "text-primary",
+    itemName: "riteshtamang1027@gmail.com",
+  },
+  {
+    url: "https://www.google.com/maps/@27.73931,85.2890271,1482m/data=!3m1!1e3?entry=ttu&g_ep=EgoyMDI1MDcwOS4wIKXMDSoASAFQAw%3D%3D",
+    icon: MapPin,
+    iconColor: "text-primary",
+    itemName: "Kathmandu",
+  },
+];
