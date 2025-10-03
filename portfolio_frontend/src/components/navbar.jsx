84fafb8003315fe147fe04bb94e51ccdbbfe0428
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-10-03 09:20:13 +0545

    add navbar file

---
+++ portfolio_frontend/src/components/navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className=' flex items-center justify-between w-full px-32 py-2'>
+
+
+            {/* left side div */}
+        <div>
+          left
+        </div>
+        {/* center div */}
+        <div>
+            center
+        </div>
+
+        {/* left side dive */}
+        <div>
+            right
+        </div>
+      
+    </div>
+  )
+}
+
