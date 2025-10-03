a3431e9d2171cfa2e7f80f30f773c1545e09341a
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-10-03 20:31:11 +0545

    add some css and make it responsive

--- portfolio_frontend/src/App.jsx
+++ portfolio_frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/navbar'
 
 export default function App() {
   return (
-    <div>
+    <div className='w-full'>
       <Navbar/>
     </div>
   )
--- portfolio_frontend/src/components/navbar.jsx
+++ portfolio_frontend/src/components/navbar.jsx
@@ -1,25 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Download } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <div className=' flex items-center justify-between w-full px-32 py-2'>
-
-
-            {/* left side div */}
-        <div>
-          left
+    <div className="border-b border-gray-400">
+      <div className=" flex items-center justify-between lg:px-32 md:px-8 sm:px-16 px-8 py-2">
+        {/* left side div */}
+        <div className="font-bold lg:text-2xl md:text-xl text-gray-800">
+          Port<span className="text-sky-800">Folio</span>
         </div>
         {/* center div */}
-        <div>
-            center
+
+        <div className="md:flex items-center lg:gap-8 gap-6 hidden  font-semibold lg:text-lg">
+          {navItems.map((item, index) => (
+            <p
+              key={index}
+              className="relative cursor-pointer after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-sky-800 after:transition-all after:duration-300 hover:after:w-full hover:text-sky-600"
+            >
+              {item}
+            </p>
+          ))}
         </div>
 
         {/* left side dive */}
-        <div>
-            right
+        <div className="p-[1px] rounded-lg bg-gradient-to-r to-gray-600 from-sky-600 ">
+          <button className="flex items-center gap-1 lg:px-4 px-2 lg:py-1 md:py-0.5 rounded-lg lg:text-base text-sm font-semibold cursor-pointer bg-white text-gray-900">
+            Download CV <Download size={16} strokeWidth={2} />
+          </button>
         </div>
-      
+      </div>
     </div>
-  )
+  );
 }
 
+const navItems = ["Home", "About", "Skill", "Services", "Portfolio", "Contact"];
