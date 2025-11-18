185ffa40189dc5a63a80276b00b0ffc4e621f607
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-11-18 19:36:16 +0545

    add toggle theme in about section

--- portfolio_frontend/src/components/about_section.jsx
+++ portfolio_frontend/src/components/about_section.jsx
@@ -3,8 +3,8 @@ import { motion } from "framer-motion";
 
 export default function About_section() {
   return (
-    <div className="text-white space-y-8">
-      <p className="text-3xl md:text-4xl font-bold text-cyan-500 text-center">About Me</p>
+    <div className="text-[var(--text-primary)]/90 space-y-8">
+      <p className="text-3xl md:text-4xl font-bold text-[var(--text-secondary)] text-center">About Me</p>
       <div className="flex flex-col md:flex-row items-center md:gap-24 gap-8">
         {/* LEFT SECTION - IMAGE */}
         <motion.div
@@ -28,7 +28,7 @@ export default function About_section() {
           className="md:w-2/3 space-y-4"
         >
           <p className="text-base md:text-lg font-medium opacity-90 leading-relaxed">
-            I am <span className="font-bold text-cyan-400">Ritesh Tamang</span>,
+            I am <span className="font-bold text-[var(--text-secondary)]">Ritesh Tamang</span>,
             a passionate Frontend Developer focused on building modern,
             responsive, and interactive web applications. I enjoy turning design
             ideas into functional websites using the latest web technologies.
