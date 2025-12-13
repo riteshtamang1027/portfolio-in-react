998534f83770f07dbbbfcf7d537b213a8a88f6bb
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-12-13 15:17:27 +0545

    make edit profile section responsive

--- portfolio_frontend/src/dashboard/edit_profile.jsx
+++ portfolio_frontend/src/dashboard/edit_profile.jsx
@@ -1,15 +1,25 @@
 import { Pencil, Plus } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Edit_profile() {
   const [Isopen, setIsopen] = useState(false);
+  useEffect(() => {
+    if (Isopen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [Isopen]);
 
   return (
     <div className="w-full">
       {Isopen && (
         <div
           onClick={() => setIsopen(!Isopen)}
-          className="w-full h-full inset-0 absolute z-40 bg-black/40"
+          className="fixed w-full h-full inset-0 z-30 bg-black/50"
         ></div>
       )}
       <div className="p-4 relative cursor-pointer bg-white shadow border border-gray-300 rounded-xl flex sm:flex-row flex-col items-center md:justify-between gap-8 hover:scale-105 duration-300 transition-all group max-md:px-16 max-md:gap-x-12">
@@ -46,60 +56,67 @@ export default function Edit_profile() {
       </div>
 
       {Isopen && (
-        <div className="w-100 max-h-[90vh] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 px-8 py-4 z-50 bg-gray-200/90 rounded-xl space-y-8 overflow-y-auto scroll-smooth hide-scrollbar">
-          {/* image edit section */}
-          <div className="flex items-center justify-center">
-            <img
-              className="rounded-full w-64 h-64  object-cover"
-              src="https://images.unsplash.com/photo-1761839258044-e59f324b5a7f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
-              alt="Image"
-            />
-          </div>
-
-          {/* your infotmation edid section */}
-          <div className="space-y-4">
-            {/* you can change your name */}
-            <div className="space-y-1 flex flex-col">
-              <label className="text-lg font-semibold ">Your Name:</label>
-              <input
-                type="text"
-                value={`Ritesh Tamang`}
-                className="border-b border-gray-600 rounded-sm px-2 py-1 focus:outline-none"
-                placeholder="Ritesh Tamang"
+        <div className="md:w-100 w-max max-h-[90vh] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 bg-gray-300 rounded-xl  overflow-y-auto scroll-smooth hide-scrollbar">
+          <p className="text-xl max-sm:text-lg font-bold px-4 py-2 border-b border-gray-500  h-full sticky top-0 bg-gray-300">
+            Edit Your profile
+          </p>
+          <div className="px-8 py-4 space-y-8 ">
+            {/* image edit section */}
+            <div className="flex items-center justify-center">
+              <img
+                className="rounded-full w-64 h-64  object-cover"
+                src="https://images.unsplash.com/photo-1761839258044-e59f324b5a7f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
+                alt="Image"
               />
             </div>
 
-            {/* add skill */}
+            {/* your infotmation edid section */}
             <div className="space-y-4">
-              <label className="text-lg font-semibold ">Skills:</label>
-              <ul className="list-disc list-inside ml-2">
-                <li>Frontend Development</li>
-                <li>UI/UX Design</li>
-                <li>React.js</li>
-              </ul>
-              <button className="flex items-center justify-center border border-gray-500 rounded-md font-semibold text-sm px-2 py-1 gap-2 cursor-pointer">
-                <span>add</span>
-                <Plus size={16} />{" "}
+              {/* you can change your name */}
+              <div className="space-y-1 flex flex-col">
+                <label className="text-lg max-sm:text-base font-semibold ">Your Name:</label>
+                <input
+                  type="text"
+                  value={`Ritesh Tamang`}
+                  className="border-b border-gray-600 max-sm:text-sm rounded-sm px-2 py-1 focus:outline-none"
+                  placeholder="Ritesh Tamang"
+                />
+              </div>
+
+              {/* add skill */}
+              <div className="space-y-4">
+                <label className="text-lg max-sm:text-base font-semibold ">Skills:</label>
+                <ul className="list-disc list-inside max-sm:text-sm ml-2">
+                  <li>Frontend Development</li>
+                  <li>UI/UX Design</li>
+                  <li>React.js</li>
+                </ul>
+                <button className="flex items-center justify-center border border-gray-500 rounded-md font-semibold text-sm px-2 py-1 gap-2 cursor-pointer">
+                  <span>add</span>
+                  <Plus size={16} />{" "}
+                </button>
+              </div>
+
+              {/* add breef about yourself */}
+              <div className="space-y-1 flex flex-col">
+                <label className="text-lg max-sm:text-base font-semibold ">
+                  Add your brief information:
+                </label>
+                <textarea
+                  type="text"
+                  className="border-b text-sm border-gray-600 max-sm:text-sm rounded-sm px-1 py-1 focus:outline-none"
+                  placeholder="Add information"
+                />
+              </div>
+            </div>
+
+            {/* submit button */}
+
+            <div className="w-full flex justify-end ">
+              <button className="border border-gray-500 px-3 py-1 rounded-md font-semibold cursor-pointer max-sm:text-sm">
+                Update
               </button>
             </div>
-
-            {/* add breef about yourself */}
-            <div className="space-y-1 flex flex-col">
-              <label className="text-lg font-semibold ">Your Name:</label>
-              <textarea
-                type="text"
-                className="border-b border-gray-600 rounded-sm px-1 py-1 focus:outline-none"
-                placeholder="Add breef your information"
-              />
-            </div>
-          </div>
-
-          {/* submit button */}
-
-          <div className="w-full flex justify-end ">
-            <button className="border border-gray-500 px-3 py-1 rounded-md font-semibold cursor-pointer">
-              Update
-            </button>
           </div>
         </div>
       )}
