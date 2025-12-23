adfd77a716e58afc0f592752ae71d2a253726bf2
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-12-23 16:59:49 +0545

    change domain name

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://portfoliobasedonreactjs.vercel.app",
+      "https://riteshtamangportfolio.vercel.app",
     ],
     credentials: true,
 
