57ee66642bc149bbd54ba169d643f79b6ba9a732
Ritesh Tamang <riteshtmglama001@gmail.com>
2025-12-25 11:20:13 +0545

    add skill add section in skill section

--- portfolio_frontend/src/dashboard/addSkillsSection.jsx
+++ portfolio_frontend/src/dashboard/addSkillsSection.jsx
@@ -1,26 +1,61 @@
 import { Plus } from "lucide-react";
-import React, { useState } from "react";
+import { useState } from "react";
 
 export default function Add_skills_section() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [skills, setskills] = useState([""]);
+  const [isAddingSkills, setIsAddingSkills] = useState(false);
+  const [skills, setSkills] = useState(["Frontend Developer"]);
   const [newSkill, setnewSkill] = useState("");
   return (
-    <div>
+    <div className="space-y-4">
       <div className="space-y-4">
-        <label className="text-lg max-sm:text-base font-semibold ">
-          Skills:
-        </label>
+        <div className="text-lg max-sm:text-base font-semibold ">Skills:</div>
         <ul className="list-disc list-inside max-sm:text-sm ml-2">
-          <li>Frontend Development</li>
+          {skills.map((skill, index) => (
+            <li key={index}>{skill}</li>
+          ))}
         </ul>
-        <label className="flex items-center justify-center border border-gray-500 rounded-md font-semibold text-sm px-2 py-1 gap-2 cursor-pointer w-max">
-          <span value={skills} onChange={(e) => setSkills(e.target.value)}>
-            add
-          </span>
-          <Plus size={16} />{" "}
-        </label>
       </div>
+
+      {isAddingSkills ? (
+        <div className="flex items-center justify-between max-sm:gap-4">
+          <input
+            type="text"
+            name=""
+            id=""
+            className="outline-none border border-gray-400 rounded-md px-2 py-1 text-sm"
+          />
+          <button
+            onClick={() => {
+              if (newSkill.trim()) {
+                setSkills(...skills, newSkill);
+                setnewSkill("");
+                setIsAddingSkills(!isAddingSkills);
+              }
+            }}
+            className="border border-gray-300 rounded-md  px-2 py-1 bg-green-500 text-white cursor-pointer text-sm"
+          >
+            Save
+          </button>
+          <button
+            onClick={() => {
+              setnewSkill("");
+              setIsAddingSkills(!isAddingSkills);
+            }}
+            className="border border-gray-300 rounded-md  px-2 py-1 bg-orange-500 text-white cursor-pointer text-sm"
+          >
+            Cancel
+          </button>
+        </div>
+      ) : (
+        /*skills Add btn */
+        <button
+          onClick={() => setIsAddingSkills(true)}
+          className="flex items-center justify-center border border-gray-500 rounded-md font-semibold text-sm px-2 py-1 gap-2 cursor-pointer w-max"
+        >
+          <span>add</span>
+          <Plus size={16} />{" "}
+        </button>
+      )}
     </div>
   );
 }
--- portfolio_frontend/src/dashboard/edit_profile.jsx
+++ portfolio_frontend/src/dashboard/edit_profile.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Pencil, Plus } from "lucide-react";
+import { Pencil } from "lucide-react";
 import { useEffect, useState } from "react";
 import Add_skills_section from "./addSkillsSection";
 
@@ -18,23 +18,22 @@ export default function Edit_profile() {
   const [uname, setUname] = useState("");
   const [info, setInfo] = useState("");
   const [img, setImg] = useState(null);
-  const [skills, setSkills] = useState("");
-
+  // const [skills, setSkills] = useState([""]);
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-  
+    e.preventDefault();
 
     try {
-      const  formdata =new FormData()
-    formdata.append("user_name",uname)
-    formdata.append("skills",skills)
-    formdata.append("profile_picture",img)
-    formdata.append("description",info)
-      const responsive = await axios.post("http://localhost:5174/admin",formdata);
-      console.log(responsive.data.data)
-
-
+      const formdata = new FormData();
+      formdata.append("user_name", uname);
+      // formdata.append("skills", skills);
+      formdata.append("profile_picture", img);
+      formdata.append("description", info);
+      const responsive = await axios.post(
+        "http://localhost:5174/admin",
+        formdata
+      );
+      console.log(responsive.data.data);
     } catch (error) {
       console.log(error);
     }
@@ -42,7 +41,6 @@ export default function Edit_profile() {
 
   return (
     <div className="w-full">
-
       {/* This is the empty div. this is needed for only to make another dive look greate. */}
       {Isopen && (
         <div
@@ -50,7 +48,6 @@ export default function Edit_profile() {
           className="fixed w-full h-full inset-0 z-30 bg-black/50"
         ></div>
       )}
-
 
       {/* This is main div which is abailable in dashboard page */}
       <div className="p-4 relative cursor-pointer bg-white shadow border border-gray-300 rounded-xl flex sm:flex-row flex-col items-center md:justify-between gap-8 hover:scale-105 duration-300 transition-all group max-md:px-16 max-md:gap-x-12">
@@ -86,38 +83,51 @@ export default function Edit_profile() {
         </div>
       </div>
 
-{/* This is the actual div where you can edit your profile's elements */}
+      {/* This is the actual div where you can edit your profile's elements */}
       {Isopen && (
-        <form onSubmit={handleSubmit} className="md:w-100 w-max max-h-[90vh] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 bg-gray-200 rounded-xl  overflow-y-auto scroll-smooth hide-scrollbar">
+        <form
+          onSubmit={handleSubmit}
+          className="md:w-100 w-max max-h-[90vh] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 bg-gray-200 rounded-xl  overflow-y-auto scroll-smooth hide-scrollbar"
+        >
           <p className="text-xl max-sm:text-lg w-full font-bold px-4 py-2 border-b border-gray-500  h-full sticky top-0 bg-gray-200">
             Edit Your profile
           </p>
           <div className="px-8 py-4 space-y-8 ">
             {/* image edit section */}
-           <div className="relative w-full   py-4">
-            {/* this input section helps you to change your profile picture */}
-            <input type="file" onChange={(e)=>setImg(e.target.files[0])} name="" id="imageUpload" className="hidden" accept="image/*" />
-
-{/* when you click this section you pushout to your device files section. */}
-            <label htmlFor="imageUpload"  className="p-2 bg-gray-300 rounded-full w-max absolute transform left-2/3 bottom-6 cursor-pointer">
-              <Pencil strokeWidth={1} size={16} />
-            </label>
-            {/* Here is the image where you can see */}
-             <div className="flex items-center justify-center">
-              <img
-              type="file"
-              onChange={(e)=>setImg(e.target.files[0])}
-                className="rounded-full w-64 h-64 -z-10 object-cover"
-                src="https://images.unsplash.com/photo-1761839258044-e59f324b5a7f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
-                alt="Image"
+            <div className="relative w-full   py-4">
+              {/* this input section helps you to change your profile picture */}
+              <input
+                type="file"
+                onChange={(e) => setImg(e.target.files[0])}
+                name=""
+                id="imageUpload"
+                className="hidden"
+                accept="image/*"
               />
+
+              {/* when you click this section you pushout to your device files section. */}
+              <label
+                htmlFor="imageUpload"
+                className="p-2 bg-gray-300 rounded-full w-max absolute transform left-2/3 bottom-6 cursor-pointer"
+              >
+                <Pencil strokeWidth={1} size={16} />
+              </label>
+              {/* Here is the image where you can see */}
+              <div className="flex items-center justify-center">
+                <img
+                  type="file"
+                  onChange={(e) => setImg(e.target.files[0])}
+                  className="rounded-full w-64 h-64 -z-10 object-cover"
+                  src="https://images.unsplash.com/photo-1761839258044-e59f324b5a7f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D"
+                  alt="Image"
+                />
+              </div>
             </div>
-           </div>
 
             {/* your infotmation edid section */}
             <div className="space-y-4">
               {/* you can change your name */}
-              <div className="space-y-1 flex flex-col">
+              <div className="space-y-2 flex flex-col">
                 <label className="text-lg max-sm:text-base font-semibold ">
                   Your Name:
                 </label>
@@ -125,27 +135,26 @@ export default function Edit_profile() {
                 <input
                   type="text"
                   value={uname}
-                  onChange={(e)=>setUname(e.target.value)}
-                  className="border-b border-gray-600 max-sm:text-sm rounded-sm px-2 py-1 focus:outline-none"
+                  onChange={(e) => setUname(e.target.value)}
+                  className="border border-gray-600 max-sm:text-sm rounded-sm px-2 py-1 focus:outline-none"
                   placeholder="Ritesh Tamang"
                 />
               </div>
 
               {/* add skill */}
 
-             < Add_skills_section/>
-             
+              <Add_skills_section/>
 
               {/* add breef about yourself */}
-              <div className="space-y-1 flex flex-col">
+              <div className="space-y-2 flex flex-col">
                 <label className="text-lg max-sm:text-base font-semibold ">
                   Add your brief information:
                 </label>
                 <textarea
-                value={info}
-                onChange={(e)=>setInfo(e.target.value)}
+                  value={info}
+                  onChange={(e) => setInfo(e.target.value)}
                   type="text"
-                  className="border-b text-sm border-gray-600 max-sm:text-sm rounded-sm px-1 py-1 focus:outline-none"
+                  className="border text-sm border-gray-600 max-sm:text-sm rounded-sm px-1 py-1 focus:outline-none"
                   placeholder="Description must be between 100 and 400 words."
                 />
               </div>
