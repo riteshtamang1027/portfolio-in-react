98e00901fa1da9817c6dc77b7b2d2a9a792b8a4a
Ritesh Tamang <riteshtmglama001@gmail.com>
2026-04-25 15:49:30 +0545

    delete backend folder

--- backend/src/routes/contact_route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createContact } from "../controllers/contact_controller.js";
-
-const router = express.Router();
-
-router.post("/", createContact);
-
-export default router; 
