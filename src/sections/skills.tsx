32bef634f521ff213d99040d55231338550c07aa
Ritesh Tamang <riteshtmglama001@gmail.com>
2026-08-14 13:11:15 +0545

    add all remaining skills list

--- src/sections/skills.tsx
+++ src/sections/skills.tsx
@@ -1,8 +1,8 @@
-import { CodeXml } from "lucide-react";
+import { Cloud, CodeXml, Database, Server,Infinity, Terminal  } from "lucide-react";
 
 export default function Skills() {
   return (
-    <main className="bg-surface-muted px-38 py-4">
+    <main className="bg-surface px-38 py-8 mt-8 space-y-8 border-x border">
       <section className="space-y-4">
         <p className="text-secondary-text text-xs tracking-widest">Toolkit</p>
         <h2 className="text-4xl font-bold text-foreground/80 tracking-tight">
@@ -13,19 +13,23 @@ export default function Skills() {
         </p>
       </section>
 
-      <section className="flex flex-col p-6 bg-background rounded-xl w-80 border gap-4">
-        
-        <div className="flex items-center gap-2">
-          <div className="bg-surface-muted p-2 rounded-sm">
-            <CodeXml className="text-accent-icon " size={16} />
+      <div className="grid grid-cols-3 items-center gap-4">
+        {skills.map((item, index) => (
+        <section
+          key={index}
+          className="flex flex-col p-6 bg-background rounded-xl border gap-4 h-40"
+        >
+          <div className="flex items-center gap-2">
+            <div className="bg-surface-muted p-2 rounded-sm">
+              <item.icon className="text-accent-icon " size={16} />
+            </div>
+            <span className="font-semibold text-lg text-foreground/80 tracking-wide">
+              {item.label}
+            </span>
           </div>
-          <span className="font-semibold text-lg text-foreground/80 tracking-wide">
-            Frontend
-          </span>
-        </div>
 
-         <div className="flex items-center flex-wrap gap-2">
-            {skillUsed.map((skill, index) => (
+          <div className="flex items-center flex-wrap gap-2">
+            {item.tools.map((skill, index) => (
               <span
                 key={index}
                 className="px-3 py-1 rounded-lg text-xs tracking-wide text-muted-foreground border border-ring bg-surface"
@@ -34,10 +38,42 @@ export default function Skills() {
               </span>
             ))}
           </div>
-
-      </section>
+        </section>
+      ))}
+      </div>
     </main>
   );
 }
 
-const skillUsed = ["Remix", "tRPC", "Postgres", "Stripe", "Kubernetes"];
+const skills = [
+  {
+    icon: CodeXml,
+    label: "Frontend",
+    tools: ["React", "Next.js", "Tailwind", "TypeScript(TS)","JavaScript(JS)"],
+  },
+  {
+    icon: Server,
+    label: "Backend",
+    tools: ["Node.js", "GraphQL", "Python", "Java", "PHP"],
+  },
+  {
+    icon: Database,
+    label: "Database",
+    tools: ["MongoDB", "Postgres", "Prisma", "SQL"],
+  },
+  {
+    icon: Cloud,
+    label: "Cloud",
+    tools: ["AWS", "Cloudflare", "Vercel", "GCP"],
+  },
+   {
+    icon: Infinity,
+    label: "DevOps",
+    tools: ["Docker", "Kubernetes", "Terraform", "GH Actions"],
+  },
+  {
+    icon: Terminal,
+    label: "Tools",
+    tools: ["Git", "Figma", "Linear", "Datadog"] ,
+  },
+];
