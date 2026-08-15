01b31a62b9cf9f4980e0aedd3f0b07abc9236a01
Ritesh Tamang <riteshtmglama001@gmail.com>
2026-08-15 22:38:36 +0545

    add more content in blog highlight file

--- src/sections/blogHighlights.tsx
+++ src/sections/blogHighlights.tsx
@@ -15,33 +15,59 @@ export default function BlogHighlights() {
 
       {/* blog cards */}
 
-      <section className="border rounded-xl w-1/3 bg-card ">
-        <div className="h-64 overflow-hidden rounded-t-xl">
-          <img
-            className="h-64 object-cover"
-            src="https://plus.unsplash.com/premium_photo-1701892428860-ca4913e92274?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGJsb2clMjBiYWNrZ3JvdW5kfGVufDB8fDB8fHww"
-            alt="Image"
-          />
-        </div>
-        <div className="px-4 py-8 flex flex-col gap-y-4">
-          <div className="flex gap-x-4">
-            <span
-              className="bg-surface-muted rounded-full px-2 py-0.5
+     <div className="flex items-center justify-between gap-8">
+         {blog.map((item, i) => (
+        <section className="border rounded-xl w-1/3 bg-card cursor-pointer">
+          <div key={i} className="h-64 overflow-hidden rounded-t-xl">
+            <img className="h-64 w-full object-cover" src={item.image} alt="Image" />
+          </div>
+          <div className="px-4 py-8 flex flex-col gap-y-4">
+            <div className="flex gap-x-4">
+              <span
+                className="bg-surface-muted rounded-full px-2 py-0.5
                 text-xs font-semibold text-foreground/80"
-            >
-              Engineering
-            </span>{" "}
-            <span className="text-xs  text-muted-foreground">8 min read</span>
+              >
+                {item.type}
+              </span>{" "}
+              <span className="text-xs  text-muted-foreground">
+                {item.time}
+              </span>
+            </div>
+            <p className="text-lg font-semibold text-foreground/80">
+              {item.title}
+            </p>
+            <div className="flex items-center gap-x-4">
+              <span>Read aritcle</span>
+              <MoveRight size={16} />
+            </div>
           </div>
-          <p className="text-lg font-semibold text-foreground/80">
-            Designing resilient server actions in modern React
-          </p>
-          <div className="flex items-center gap-x-4">
-            <span>Read aritcle</span>
-            <MoveRight size={16} />
-          </div>
-        </div>
-      </section>
+        </section>
+      ))}
+     </div>
     </main>
   );
 }
+
+const blog = [
+  {
+    image:
+      "https://plus.unsplash.com/premium_photo-1701892428860-ca4913e92274?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGJsb2clMjBiYWNrZ3JvdW5kfGVufDB8fDB8fHww",
+    type: "Engineering",
+    time: "8 min read",
+    title: " Designing resilient server actions in modern React",
+  },
+  {
+    image:
+      "https://plus.unsplash.com/premium_vector-1734662648778-ca3ca43e7739?w=352&dpr=2&h=367&auto=format&fit=crop&q=60&ixlib=rb-4.1.0",
+    type: "Database",
+    time: "12 min read",
+    title: " Scaling Postgres to 10 billion rows without losing your weekend",
+  },
+  {
+    image:
+      "https://plus.unsplash.com/premium_photo-1720287601920-ee8c503af775?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW5mcmFzdHJ1Y3R1cmUlMjBkZXZlbG9wbWVudCUyMHNvZnR3YXJlfGVufDB8fDB8fHww",
+    type: "Infrastructure",
+    time: "6 min read",
+    title: " A pragmatic guide to multi-region deploys on Cloudflare",
+  },
+];
